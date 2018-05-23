c06da2adbf371800348a25b607fb363d204dbc87
Simon Gabriel <simon_gabriel@gmx.de>
2018-05-23 13:23:57 +0200

    [bugfix] (PLENTY-3) Debug: Add some log outputs to find an error.

--- src/Configs/MethodConfig.php
+++ src/Configs/MethodConfig.php
@@ -21,9 +21,12 @@ use Heidelpay\Methods\PaymentMethodContract;
 use Heidelpay\Methods\PayPal;
 use Heidelpay\Methods\Prepayment;
 use Heidelpay\Methods\Sofort;
+use Plenty\Plugin\Log\Loggable;
 
 class MethodConfig extends BaseConfig implements MethodConfigContract
 {
+    use Loggable;
+
     const ARRAY_KEY_CONFIG_KEY = 'config_key';
     const ARRAY_KEY_DEFAULT_NAME = 'default_name';
     const ARRAY_KEY_KEY = 'key';
@@ -123,7 +126,10 @@ class MethodConfig extends BaseConfig implements MethodConfigContract
      */
     public function getPaymentMethodName(PaymentMethodContract $paymentMethod): string
     {
-        return $this->get($this->getDisplayNameKey($paymentMethod));
+        $name = $this->get($this->getDisplayNameKey($paymentMethod));
+        $this->getLogger(__METHOD__)->error($name);
+        $this->getLogger(__METHOD__)->error($paymentMethod->getDefaultName());
+        return $name;
     }
 
     /**

4cad45336fa299fd4afa57ef2f43f70ea60ab655
Simon Gabriel <simon_gabriel@gmx.de>
2018-05-28 14:58:55 +0200

    Revert "[bugfix] (PLENTY-3) Add Debit Card to PaymentService."
    This reverts commit e53d736

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -10,7 +10,6 @@ use Heidelpay\Constants\SessionKeys;
 use Heidelpay\Constants\TransactionStatus;
 use Heidelpay\Helper\PaymentHelper;
 use Heidelpay\Methods\CreditCard;
-use Heidelpay\Methods\DebitCard;
 use Heidelpay\Methods\PaymentMethodContract;
 use Heidelpay\Methods\PayPal;
 use Heidelpay\Methods\Prepayment;
@@ -49,8 +48,6 @@ use Plenty\Plugin\Templates\Twig;
 class PaymentService
 {
     use Loggable;
-
-    const METHODS_USING_IFRAME = [CreditCard::class, DebitCard::class];
 
     /**
      * @var string
@@ -272,12 +269,6 @@ class PaymentService
                 $result = $this->sendGetPaymentMethodContentRequest($basket, $instance, $mopId);
                 break;
 
-            case DebitCard::class:
-                $instance = pluginApp(DebitCard::class);
-                $this->setReturnType(GetPaymentMethodContent::RETURN_TYPE_HTML);
-                $result = $this->sendGetPaymentMethodContentRequest($basket, $instance, $mopId);
-                break;
-
             case PayPal::class:
                 $instance = pluginApp(PayPal::class);
                 $this->setReturnType(GetPaymentMethodContent::RETURN_TYPE_REDIRECT_URL);
@@ -331,9 +322,8 @@ class PaymentService
                     return $result['response']['PROCESSING.RETURN'];
                 }
 
-                // Credit Card and Debit Card use an iframe the other methods use redirect to the payment server.
-                $urlKey = \in_array($paymentMethod, self::METHODS_USING_IFRAME, true) ?
-                    'FRONTEND.PAYMENT_FRAME_URL' : 'FRONTEND.REDIRECT_URL';
+                // todo: extend for DebitCard
+                $urlKey = $paymentMethod === CreditCard::class ? 'FRONTEND.PAYMENT_FRAME_URL' : 'FRONTEND.REDIRECT_URL';
 
                 return $this->twig->render('heidelpay::externalCardForm', [
                     'paymentFrameUrl' => $result['response'][$urlKey]
@@ -402,7 +392,7 @@ class PaymentService
 
         // add the origin domain, which is important for the CSP
         // set 'PREVENT_ASYNC_REDIRECT' to false, to ensure the customer is being redirected after submitting the form.
-        if (\in_array($paymentMethod, self::METHODS_USING_IFRAME, true)) {
+        if ($paymentMethod === CreditCard::class) {
             $this->heidelpayRequest['FRONTEND_PAYMENT_FRAME_ORIGIN'] = $this->paymentHelper->getDomain();
             $this->heidelpayRequest['FRONTEND_PREVENT_ASYNC_REDIRECT'] = 'false';
         }

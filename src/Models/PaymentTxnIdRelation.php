b3bcc256377b9f4880c4811b7bcb99ebf4d73cb9
Simon Gabriel <simon_gabriel@gmx.de>
2018-06-06 09:21:54 +0200

    [bugfix] (PLENTY-3) Added missing property annotations.

--- src/Models/PaymentTxnIdRelation.php
+++ src/Models/PaymentTxnIdRelation.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * heidelpay PaymentTxnIdRelation model class
+ *
+ * @license Use of this software requires acceptance of the License Agreement. See LICENSE file.
+ * @copyright Copyright © 2017-present heidelpay GmbH. All rights reserved.
+ *
+ * @link http://dev.heidelpay.com/plentymarkets-gateway
+ *
+ * @author Simon Gabriel <development@heidelpay.com>
+ *
+ * @package heidelpay\plentymarkets-gateway\models
+ *
+ * @property int $id
+ * @property string $txnId
+ * @property string $paymentId
+ * @property string $createdAt
+ * @property string $updatedAt
+ * @property string $assignedAt
+ */
 namespace Heidelpay\Models;
 
 use Heidelpay\Constants\Plugin;
@@ -9,7 +28,7 @@ use Plenty\Modules\Plugin\DataBase\Contracts\Model;
  */
 class PaymentTxnIdRelation extends Model
 {
-    const TABLE_NAME = 'transactionpaymentrelations';
+    const TABLE_NAME = 'transaction_payment_relations';
 
     const FIELD_ID = 'id';
     const FIELD_CREATED_AT = 'createdAt';

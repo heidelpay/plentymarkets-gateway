a8e4717e2ce75693dcd904044a1af926cb38c62b
Simon Gabriel <simon_gabriel@gmx.de>
2018-06-07 13:24:46 +0200

    [debug] (PLENTY-3) order txnId relation: Changed primary key to default.

--- src/Models/OrderTxnIdRelation.php
+++ src/Models/OrderTxnIdRelation.php
@@ -20,6 +20,7 @@ use Plenty\Modules\Plugin\DataBase\Contracts\Model;
  * heidelpay OrderTxnIdRelation model class
  *
  * @property int $orderId
+ * @property int $id
  * @property string $txnId
  * @property int $mopId
  * @property string $assignedAt
@@ -28,6 +29,7 @@ use Plenty\Modules\Plugin\DataBase\Contracts\Model;
  */
 class OrderTxnIdRelation extends Model
 {
+    const FIELD_ID = 'id';
     const FIELD_ORDER_ID = 'orderId';
     const FIELD_TXN_ID = 'txnId';
     const FIELD_MOP_ID = 'mopId';
@@ -35,6 +37,7 @@ class OrderTxnIdRelation extends Model
     const FIELD_CREATED_AT = 'createdAt';
     const FIELD_UPDATED_AT = 'updatedAt';
 
+    public $id;
     public $orderId;
     public $txnId;
     public $mopId;
@@ -42,7 +45,7 @@ class OrderTxnIdRelation extends Model
     public $createdAt;
     public $updatedAt;
 
-    protected $primaryKeyFieldName = self::FIELD_ORDER_ID;
+//    protected $primaryKeyFieldName = self::FIELD_ORDER_ID;
 
     /**
      * @return string

3f446939a2d7d8ce22b49e30a5cb9dff72cc3801
Simon Gabriel <simon_gabriel@gmx.de>
2018-07-18 12:40:59 +0200

    [refactor] (PLENTY-3) Refactored variable names.

--- src/Services/ArraySerializerService.php
+++ src/Services/ArraySerializerService.php
@@ -20,30 +20,30 @@ class ArraySerializerService
     const SERIALIZER_KEY_VALUE_DELIMITER = ': ';
 
     /**
-     * @param array $bookingTextArray
+     * @param array $arrayToSerialize
      * @return string
      */
-    public function serializeKeyValue(array $bookingTextArray): string
+    public function serializeKeyValue(array $arrayToSerialize): string
     {
         return implode(self::SERIALIZER_PAIR_DELIMITER, array_map(
             function ($value, $key) {
                 return $key . self::SERIALIZER_KEY_VALUE_DELIMITER . $value;
             },
-            $bookingTextArray,
-            array_keys($bookingTextArray)
+            $arrayToSerialize,
+            array_keys($arrayToSerialize)
         ));
     }
 
     /**
      * Creates an array from a string serialized with the serializeKeyValue method.
      *
-     * @param string $bookingText
+     * @param string $serializedString
      * @return array
      */
-    public function deserializeKeyValue($bookingText): array
+    public function deserializeKeyValue($serializedString): array
     {
         $resultArray = [];
-        foreach (explode(self::SERIALIZER_PAIR_DELIMITER, $bookingText) as $element) {
+        foreach (explode(self::SERIALIZER_PAIR_DELIMITER, $serializedString) as $element) {
             list($key, $value) = explode(self::SERIALIZER_KEY_VALUE_DELIMITER, $element);
             $resultArray[$key] = $value;
         }

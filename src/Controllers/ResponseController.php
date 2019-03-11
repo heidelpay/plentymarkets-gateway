1bf1fb933c83b07034f0d9bdb47f81712f29dcea
Simon Gabriel <simon_gabriel@gmx.de>
2019-03-11 09:40:41 +0100

    [feature] (PLENTY-88) NGW-IVsec: Fix return salutation.

--- src/Controllers/ResponseController.php
+++ src/Controllers/ResponseController.php
@@ -121,7 +121,7 @@ class ResponseController extends Controller
     private function getSalutation(): string
     {
         if ($this->request->exists('customer_salutation')) {
-            $this->request->get('customer_salutation');
+            return $this->request->get('customer_salutation');
         }
 
         throw new \RuntimeException('Salutation not set!');
@@ -244,11 +244,6 @@ class ResponseController extends Controller
         BasketRepositoryContract $basketRepo,
         PaymentHelper $paymentHelper
     ): BaseResponse {
-
-        $this->notification->error('payment.errorDuringPaymentExecution', __METHOD__, $this->request->all());
-        return $this->response->redirectTo('checkout');
-
-
         $basket = $basketRepo->load();
 
         $mopId          = $basket->methodOfPaymentId;

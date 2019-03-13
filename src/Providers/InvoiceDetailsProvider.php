4d4345b8d3f1bcc5d8b08ae698df0117a04fbc3c
Simon Gabriel <simon_gabriel@gmx.de>
2019-03-13 11:07:59 +0100

    [feature] (PLENTY-88) NGW-IVsec: Remove debug notifications.

--- src/Providers/InvoiceDetailsProvider.php
+++ src/Providers/InvoiceDetailsProvider.php
@@ -3,7 +3,6 @@ namespace Heidelpay\Providers;
 
 use Heidelpay\Constants\SessionKeys;
 use Heidelpay\Models\Contracts\TransactionRepositoryContract;
-use Heidelpay\Services\NotificationServiceContract;
 use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
 use Plenty\Plugin\Templates\Twig;
 
@@ -11,29 +10,17 @@ class InvoiceDetailsProvider
 {
     public function call(
         Twig $twig,
-        NotificationServiceContract $notificationService,
         FrontendSessionStorageFactoryContract $sessionStorage,
         TransactionRepositoryContract $transactionRepos
     ): string {
-        // render bank details
         $txnId = $sessionStorage->getPlugin()->getValue(SessionKeys::SESSION_KEY_TXN_ID);
         $transaction = $transactionRepos->getTransactionsByTxnId($txnId)[0];
-
-        $notificationService->error('remove me ', __METHOD__, ['txn' => $transaction]);
 
         $details = $transaction->transactionDetails;
         $accountIBAN = $details['CONNECTOR.ACCOUNT_IBAN'];
         $accountBIC = $details['CONNECTOR.ACCOUNT_BIC'];
         $accountHolder = $details['CONNECTOR.ACCOUNT_HOLDER'];
         $accountUsage = $details['CONNECTOR.ACCOUNT_USAGE'] ?? $transaction->shortId;
-
-        $notificationService->error('remove me too', __METHOD__, [
-            'accountIBAN' => $accountIBAN,
-            'accountBIC' => $accountBIC,
-            'accountHolder' => $accountHolder,
-            'accountUsage' => $accountUsage,
-            'shortId' => $transaction->shortId
-        ]);
 
         return $twig->render(
             'Heidelpay::content/InvoiceDetails',

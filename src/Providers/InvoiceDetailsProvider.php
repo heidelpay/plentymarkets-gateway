580bc0a6d422fc87ce1f120eecda0c207ae276b4
Simon Gabriel <simon_gabriel@gmx.de>
2019-03-27 10:40:28 +0100

    [change] (PLENTY-107) Debug: Show arguments of data provider in log.

--- src/Providers/InvoiceDetailsProvider.php
+++ src/Providers/InvoiceDetailsProvider.php
@@ -4,16 +4,29 @@ namespace Heidelpay\Providers;
 use Heidelpay\Constants\SessionKeys;
 use Heidelpay\Helper\PaymentHelper;
 use Heidelpay\Methods\PaymentMethodContract;
+use Heidelpay\Services\NotificationServiceContract;
 use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
 use Plenty\Plugin\Templates\Twig;
 
 class InvoiceDetailsProvider
 {
+    /**
+     * @param Twig $twig
+     * @param FrontendSessionStorageFactoryContract $sessionStorage
+     * @param PaymentHelper $helper
+     * @param NotificationServiceContract $notificationService
+     * @param array $args
+     * @return string
+     */
     public function call(
         Twig $twig,
         FrontendSessionStorageFactoryContract $sessionStorage,
-        PaymentHelper $helper
+        PaymentHelper $helper,
+        NotificationServiceContract $notificationService,
+        $args
     ): string {
+        $notificationService->error('Arguments: ', __METHOD__, ['ARGS' => $args]);
+
         $mopId = $sessionStorage->getOrder()->methodOfPayment;
 
         /** @var PaymentMethodContract $paymentMethod */
